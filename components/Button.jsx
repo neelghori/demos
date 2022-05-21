8cc117ae82ac597b7245931be16d0f5832bf71bf
Ghori Neel <71180766+neelghori@users.noreply.github.com>
2022-05-21 15:23:15 +0530

    Update Button.jsx

--- components/Button.jsx
+++ components/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Button = ({ label, type, disabled, onClick }) => {
   return (
     <div>
-      <button type={type} disabled={disabled} onClick={onClick}>
+      <button type={type} onClick={onClick}>
         {label}
       </button>
     </div>
